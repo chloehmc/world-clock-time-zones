82c344768eee6015b3d9989e0291e49f1e97b1de
chloehmc <122628488+chloehmc@users.noreply.github.com>
2023-02-16 12:30:07 +0100

    added open hours display

--- src/app.js
+++ src/app.js
@@ -8,6 +8,12 @@ function updateTimes() {
     const now = luxon.DateTime.now().setZone(timezone);
 
     output.innerHTML = now.toFormat("HH: mm :ss");
+
+    const hour = parseInt(now.toFormat("H"));
+
+    if (hour >= 9 && hour < 17) {
+      location.classList.add("open");
+    }
   });
 }
 updateTimes();
