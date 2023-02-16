f89895e18e752ab519ed9ade2676022b3522ea2e
chloehmc <122628488+chloehmc@users.noreply.github.com>
2023-02-16 12:11:39 +0100

    added js script for updating times using luxon

--- src/app.js
+++ src/app.js
@@ -1,1 +1,17 @@
+const locations = document.querySelectorAll("section.times div");
 
+function updateTimes() {
+  locations.forEach((location) => {
+    const output = location.querySelector("output");
+    const timezone = location.getAttribute("data-timezone");
+
+    const now = luxon.DateTime.now().setZone(timezone);
+
+    output.innerHTML = now.toFormat("HH: mm :ss");
+  });
+}
+updateTimes();
+
+setInterval(function () {
+  updateTimes();
+}, 1000);
